110b3dcffbcab940c06d47d8c214e817d4e080a5
AdiatsaPutra <santikosetyo77@gmail.com>
2021-05-22 12:23:51 +0700

    fixing kategori

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,6 @@
         <div class="col-md-3">
           <div class="card border-bottom-primary border-bottom-5 mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." alt="...">
-              </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">Stok Obat</h5>
@@ -20,13 +17,10 @@
         <div class="col-md-3">
           <div class="card border-bottom-danger border-bottom-5 mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." alt="...">
-              </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">Obat Merah</h5>
-                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'obat merah')->count()}}</h1>
+                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'Obat Merah')->count()}}</h1>
                 </div>
               </div>
             </div>
@@ -35,13 +29,10 @@
         <div class="col-md-3">
           <div class="card border-bottom-warning border-bottom-5 mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." alt="...">
-              </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">Obat Kuning</h5>
-                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'obat kuning')->count()}}</h1>
+                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'Obat Kuning')->count()}}</h1>
                 </div>
               </div>
             </div>
@@ -50,13 +41,10 @@
         <div class="col-md-3">
           <div class="card border-bottom-success border-bottom-5 mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="..." alt="...">
-              </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">Obat Hijau</h5>
-                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'obat hijau')->count()}}</h1>
+                  <h1 class="fw-bold">{{App\Models\Obat::get(['kategori'])->where('kategori', 'Obat Hijau')->count()}}</h1>
                 </div>
               </div>
             </div>
@@ -86,7 +74,12 @@
                 </div>
                 <div class="mb-3">
                   <label for="kategori" class="form-label">Kategori Produk</label>
-                  <input class="form-control" id="kategori" name="kategori">
+                  <select class="form-select" id="kategori" name="kategori">
+                    <option selected>--Pilih Kategori--</option>
+                    <option value="Obat Merah">Obat Merah</option>
+                    <option value="Obat Kuning">Obat Kuning</option>
+                    <option value="Obat Hijau">Obat Hijau</option>
+                  </select>
                 </div>
                 <div class="mb-3">
                   <label for="harga" class="form-label">Harga Produk</label>
@@ -123,7 +116,7 @@
             </tr>
         </thead>
         <tbody>
-            @forelse ($obat as $dataObat)
+            @foreach ($obat as $dataObat)
             <tr>
                 <td>{{$loop->iteration}}</td>
                 <td>{{$dataObat->nama}}</td>
@@ -147,9 +140,7 @@
                   </div>
                 </td>
             </tr>
-            @empty
-                <h1>Belum ada data</h1>
-            @endforelse
+            @endforeach
         </tbody>
     </table>
 </div>
